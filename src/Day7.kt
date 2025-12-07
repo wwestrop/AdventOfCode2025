f5427f8d28fcc5d71ac3b4f342c821008f0e7b14
Will Westrop <wwestrop@hotmail.com>
2025-12-07 17:19:43 +0000

    Day 7, part 1 - works for sample, not for full input

---
+++ src/Day7.kt
@@ -0,0 +1,60 @@
+enum class CellType {
+    Start,
+    Splitter,
+    Space,
+}
+
+private fun parseLine(input: String): List<CellType> {
+    return input.map {
+        when (it) {
+            'S' -> CellType.Start
+            '^' -> CellType.Splitter
+            '.' -> CellType.Space
+            else -> error("Unknown cell type '$it'")
+        }
+    }
+}
+
+fun day7(rawInput: String): Long {
+    val grid = parseLines(rawInput, ::parseLine)
+        .map { it.toMutableList() }
+        .toMutableList()
+
+    var accumulator = 0L
+    val startPoint = grid.enumeratePoints().first { grid[it] == CellType.Start }
+
+    val downVector = Point(0, 1)
+    var beamFrontier = setOf(startPoint + downVector)
+    val allBeams = beamFrontier.toMutableSet()
+    while (beamFrontier.any()) {
+        val newBeams = mutableSetOf<Point>()
+
+        for (beam in beamFrontier) {
+            if (grid[beam] == CellType.Splitter) {
+                // spawn the new beams
+                val leftBeam = beam + Point(-1, 0)
+                if (!grid.isOutOfBounds(leftBeam) && !allBeams.contains(leftBeam)) {    // don't emit another beam into a beam passing parallel (since the "frontier" for that path has already moved on)
+                    newBeams.add(leftBeam)
+                }
+
+                val rightBeam = beam + Point(1, 0)
+                if (!grid.isOutOfBounds(rightBeam) && !allBeams.contains(rightBeam)) {
+                    newBeams.add(rightBeam)
+                }
+
+                accumulator++
+            }
+            else {
+                val beamContinuation = beam + downVector
+                if (!grid.isOutOfBounds(beamContinuation)) {
+                    newBeams.add(beamContinuation)
+                }
+            }
+        }
+
+        beamFrontier = newBeams.toMutableSet()
+        allBeams.addAll(newBeams)
+    }
+
+    return accumulator
+}
