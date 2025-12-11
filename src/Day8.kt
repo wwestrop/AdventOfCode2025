17716215a439df4ffd523570631910b6175b94b5
Will Westrop <wwestrop@hotmail.com>
2025-12-11 18:31:56 +0000

    Day 8, part 2

--- src/Day8.kt
+++ src/Day8.kt
@@ -52,6 +52,46 @@ fun day8(rawInput: String): Int {
         .reduce { acc, v -> acc * v }
 }
 
+fun day8part2(rawInput: String): Int {
+    val boxes = parseLinesIndexed(rawInput, ::parseLine)
+
+    val sortedDistances = findBoxDistances(boxes)
+        .sortedBy { it.second }
+        .map { it.first }
+        .toList()
+
+    var circuitCount = boxes.size
+    val uniqueCircuits = boxes
+        .groupBy { it.circuit }
+        .entries
+        .associateBy(
+            keySelector =  { it.key },
+            valueTransform = { it.value.toMutableSet() })
+
+    for ((box1, box2) in sortedDistances) {
+        // everything merges into the lower circuit number
+        val (smaller, larger) =
+            if (box1.circuit < box2.circuit) Pair(box1.circuit, box2.circuit)
+            else Pair(box2.circuit, box1.circuit)
+
+        if (smaller != larger) {
+            val moving = uniqueCircuits[larger]!!
+            for (box in moving) {
+                box.circuit = smaller
+            }
+            uniqueCircuits[smaller]!!.addAll(moving)
+            uniqueCircuits[larger]!!.clear()
+            circuitCount--
+
+            if (circuitCount == 1) {
+                return box1.position.first * box2.position.first
+            }
+        }
+    }
+
+    throw Exception("Should connect all the boxes before this happens")
+}
+
 fun findBoxDistances(boxes: List<JunctionBox>): Sequence<Pair<Pair<JunctionBox, JunctionBox>, Double>> {
     return sequence {
         for (i in 0..<boxes.count()) {
