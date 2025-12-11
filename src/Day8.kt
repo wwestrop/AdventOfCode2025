485d736c3d8962b35c9c8e363d07d6ef238bcc29
Will Westrop <wwestrop@hotmail.com>
2025-12-11 18:27:11 +0000

    Day 8, part 1 - fixed

--- src/Day8.kt
+++ src/Day8.kt
@@ -1,4 +1,3 @@
-import kotlin.math.max
 import kotlin.math.pow
 import kotlin.math.sqrt
 
@@ -13,68 +12,44 @@ private fun parseLine(i: Int, input: String): JunctionBox {
     return JunctionBox(position = Triple(parts[0], parts[1], parts[2]), circuit = i)
 }
 
-fun day8(rawInput: String): Long {
+fun day8(rawInput: String): Int {
     val boxes = parseLinesIndexed(rawInput, ::parseLine)
 
-    val distances = findBoxDistances(boxes)
-
-    val distancesDebug = distances.sortedBy { it.second }.map { "\"${it.first.first.position}\", ${it.first.first.circuit}, \"${it.first.second.position}\", ${it.first.second.circuit}, ${it.second}" }
-    val distancesDebugString = distancesDebug.joinToString("\n")
-
-    val sortedDistances = distances
+    val sortedDistances = findBoxDistances(boxes)
         .sortedBy { it.second }
-        .take(10)
-        //.take(1000)
+        .take(1000)
+        //.take(10)
         .map { it.first }
         .toList()
 
-//    val circuitContents = sortedDistances.map { it.first.circuit }.toSet()      // TODO it's possible considering only the first n will leave me with an inaccurate result, but I'm going to a assume a long tail of circuits of 1 that make no difference. YOLO
     val uniqueCircuits = boxes
         .groupBy { it.circuit }
         .entries
         .associateBy(
             keySelector =  { it.key },
             valueTransform = { it.value.toMutableSet() })
-    val rrrrr = uniqueCircuits[2]
 
-
-    val preferredCircuits = mutableSetOf<Int>()
     for ((box1, box2) in sortedDistances) {
-        // if both boxes are already in circuits, they all merge into the lower circuit
-//        if (uniqueCircuits[box1.circuit]!!.size > 1 && uniqueCircuits[box2.circuit]!!.size > 1) {
-//            // box2.circuit = box1.circuit
-//            TODO
-//        }
-
         // everything merges into the lower circuit number
         val (smaller, larger) =
             if (box1.circuit < box2.circuit) Pair(box1.circuit, box2.circuit)
             else Pair(box2.circuit, box1.circuit)
 
-        val moving = uniqueCircuits[larger]!!
-        for (box in moving) {
-            box.circuit = smaller
+        if (smaller != larger) {
+            val moving = uniqueCircuits[larger]!!
+            for (box in moving) {
+                box.circuit = smaller
+            }
+            uniqueCircuits[smaller]!!.addAll(moving)
+            uniqueCircuits[larger]!!.clear()
         }
-        uniqueCircuits[smaller]!!.addAll(moving)        // TODO no this doesn't work because I need to fiddle the circuit on all those I'm moving
-
-
-        //uniqueCircuits.remove(larger)
-        uniqueCircuits[larger]!!.clear()
-
-
-
-        // otherwise, the box in the highest circuit
-//        if (box2.circuit in preferredCircuits) {
-//            box1.circuit = box2.circuit
-//        } else {
-//            box2.circuit = box1.circuit
-//            preferredCircuits.add(box1.circuit)
-//        }
     }
-//
-//    val uniqueCircuits = boxes.groupBy { it.circuit }
 
-    return 0
+    return uniqueCircuits.entries
+        .map { it.value.size }
+        .sortedDescending()
+        .take(3)
+        .reduce { acc, v -> acc * v }
 }
 
 fun findBoxDistances(boxes: List<JunctionBox>): Sequence<Pair<Pair<JunctionBox, JunctionBox>, Double>> {
